b8814077909029a9ebfb416ef1ac43cbce24dc1b
Balkı <cavide.balki@gmail.com>
2019-10-23 20:03:31 +0300

    Update server.c

--- server.c
+++ server.c
@@ -5,28 +5,36 @@
 
 double *add_1_svc(char** msg){
     static double result;
+    printf("Remote ADD call : %s\n", *msg);
     Operands data = split(*msg);
     result = data.operand1 + data.operand2;
+    printf("Remote call result : %f\n", result);
     return(&result);
 }
 
 double **subtract_1_svc(char** msg) {
     static double result;
+    printf("Remote SUBSTRACT call : %s\n", *msg);
     Operands data = split(*msg);
     result = data.operand1 - data.operand2;
+    printf("Remote call result : %f\n", result);
     return(&result);
 }
 
 double **multiply_1_svc(char** msg) {
     static double result;
+    printf("Remote MULTIPLY call : %s\n", *msg);
     Operands data = split(*msg);
     result = data.operand1 * data.operand2;
+    printf("Remote call result : %f\n", result);
     return(&result);
 }
 
 double **divide_1_svc(char** msg) {
     static double result;
+    printf("Remote DIVIDE call : %s\n", *msg);
     Operands data = split(*msg);
     result = (double) data.operand1 / data.operand2;
+    printf("Remote call result : %f\n", result);
     return(&result);
 }
